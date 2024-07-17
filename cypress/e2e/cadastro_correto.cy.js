5facb1cbf198c8c7516dc6484854c33e5e8ddf68
Guilherme Ferreira <guilherme.ferreira@opinionbox.com>
2024-07-17 10:21:48 -0300

    subindo gitignore com .env

---
+++ cypress/e2e/cadastro_correto.cy.js
@@ -0,0 +1,13 @@
+describe('Página de cadastro', () => {
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app');
+
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Guilherme Ferreira Costa');
+    cy.get('[data-test="input-email"]').type('guilherme.ferreira4@email.com');
+    cy.get('[data-test="input-password"]').type('Senha123');
+    cy.get('[data-test="input-confirm-password"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
+
+  })
+})
